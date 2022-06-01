3219294843b815a68e5ee33945019207d2ad8d06
ajzia <jokul502@wp.pl>
2022-06-01 18:45:06 +0200

    Ex. 1,2 from list 8.

---
+++ lista8/zad1.pl
@@ -0,0 +1,26 @@
+%   compiling:  |   usage:
+%   ?- [zad1].  |   ?- coloring().
+
+% ?- coloring(X), label(X).
+% X = [1, 2, 1, 3, 2, 3, 1, 2, 3] ;
+% X = [1, 3, 1, 2, 3, 2, 1, 3, 2] ;
+% X = [2, 1, 2, 3, 1, 3, 2, 1, 3] ;
+% X = [2, 3, 2, 1, 3, 1, 2, 3, 1] ;
+% X = [3, 1, 3, 2, 1, 2, 3, 1, 2] ;
+% X = [3, 2, 3, 1, 2, 1, 3, 2, 1].
+
+:- use_module(library(clpfd)).
+
+coloring(X) :-
+  X = [R1,R2,R3,R4,R5,R6,R7,R8,R9],
+  X ins 1..3,
+  R1 #\= R2, R1 #\= R4, R1 #\= R5, R1 #\= R6,
+  R2 #\= R3, R2 #\= R4, R2 #\= R9,
+  R3 #\= R4, R3 #\= R5, R3 #\= R9,
+  R4 #\= R5,
+  R5 #\= R6, R5 #\= R7, R5 #\= R9,
+  R6 #\= R7, R6 #\= R8,
+  R7 #\= R8, R7 #\= R9,
+  R8 #\= R9.
+
+% Map of region and its neighbours, then assign colors 
---
+++ lista8/zad2.pl
@@ -0,0 +1,28 @@
+% Napisz w SWI-Prologu z wykorzystaniem programowania ograniczen predykat
+% plecak(+Wartosci, +Wielkosci, +Pojemnosc, -Zmienne), ktory dla danej
+% listy wartosci przedmiotow, listy wielkosci przedmiotow i pojemnosci plecaka,
+% rozwiązuje dyskretny problem plecakowy.
+
+%   compiling:  |   usage:
+%   ?- [zad2].  |   ?- backpack(Values, Weights, Capacity, Vars).
+%               |   ! Values and Weights have to have the same length. !
+
+% ?- backpack([10,7,1,3,2],[9,12,2,7,5],15,X).
+% X = [1, 0, 0, 0, 1].
+%
+% ?- backpack([6,5,4,3,2,1],[6,1,5,2,4,3],10,X).
+% X = [1, 1, 0, 1, 0, 0].
+%
+% ?- backpack([15,30,18,9,2,8,9,5,17],[9,6,11,8,19,20,12,7,5],50,X).
+% X = [1, 1, 1, 0, 0, 0, 1, 1, 1].
+
+:- use_module(library(clpfd)).
+
+backpack(Values, Weights, Capacity, Vars) :-
+  length(Values, L),
+  length(Weights, L),
+  length(Vars, L),
+  scalar_product(Values, Vars, #= , Goal),  % scalar product, Values[1] * Vars[1] + ... + Values[n] * Vars[n] #= Goal (which is maximalized by using max(Obj))
+  scalar_product(Weights, Vars, #=<, Capacity),
+  Vars ins 0..1,
+  once(labeling([max(Goal)], Vars)).
