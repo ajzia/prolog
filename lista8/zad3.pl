e34b26e6290aa8f40d61d48f414e544495526054
ajzia <jokul502@wp.pl>
2022-06-09 23:02:25 +0200

    Additional implementation.

--- lista8/zad3.pl
+++ lista8/zad3.pl
@@ -1,9 +1,8 @@
-:- use_module(library(clpfd)).
-
 %   compiling:  |   usage:
-%   ?- [zad3].  |   ?- interval(L), label(L), writeln(L), fail.
+%   ?- [zad3].  |   ?- linear_interval(L), label(L), writeln(L), fail.
+%               |   ?- interval(L), label(L), writeln(L), fail.
 
-% ?- interval(L), label(L), writeln(L), fail.
+% ?- linear_interval(L), label(L), writeln(L), fail.
 % [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]
 % [0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0]
 % [0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0]
@@ -13,21 +12,35 @@
 % [0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0]
 % [0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0]
 % [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0]
-% false.
+% false. */
+
+:- use_module(library(clpfd)).
+
+bound(L, M) :-
+  L = [X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, X11, X12, X13, X14, X15, X16],
+  % implications expressed by inequalities (1 - X) * M >= Y, where M - maximum number of ones
+  ((1-X1) * M #>= X9 + X10 + X11 + X12 + X13 + X14 + X15 + X16),
+  ((1-X2) * M #>= X10 + X11 + X12 + X13 + X14 + X15 + X16),
+  ((1-X3) * M #>= X11 + X12 + X13 + X14 + X15 + X16),
+  ((1-X4) * M #>= X12 + X13 + X14 + X15 + X16),
+  ((1-X5) * M #>= X13 + X14 + X15 + X16),
+  ((1-X6) * M #>= X14 + X15 + X16),
+  ((1-X7) * M #>= X15 + X16),
+  ((1-X8) * M #>= X16).
+
+% implementation using linear bounds 
+linear_interval(List) :-
+  length(List, 16), 
+  List ins 0..1, 
+  M #= 8,
+  sum(List, #=, M), 
+  bound(List, M).
 
 interval(List) :-
-  length(List, 16), % (length(List, X), X #= 16)
-  List ins 0..1, % domain 0..1
-  sum(List, #=, 8), % sum of elements on list #' 8'
-  length(X, 8), % (length(X, Y), Y #= 8)
+  length(List, 16), 
+  List ins 0..1, 
+  sum(List, #=, 8), 
+  length(X, 8), 
   append(X, Y, List),
-  chain(X, #=<), % linear?
-  chain(Y, #>=). % linear?
-
-% ?
-odcinek(List):-
-  length(List, 16),
-  List ins 0..1,
-  sum(List, #=, 8),
-  append(W, _, List),
-  append(_, [1,1,1,1,1,1,1,1], W).
+  chain(X, #=<),
+  chain(Y, #>=).
