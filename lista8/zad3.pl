fd1bae4172350752947e0cefcaddd942da72b218
ajzia <jokul502@wp.pl>
2022-06-10 22:23:34 +0200

    linear_interval working for different M values.

--- lista8/zad3.pl
+++ lista8/zad3.pl
@@ -2,6 +2,7 @@
 %   ?- [zad3].  |   ?- linear_interval(L), label(L), writeln(L), fail.
 %               |   ?- interval(L), label(L), writeln(L), fail.
 
+% Output for M #= 8 :
 % ?- linear_interval(L), label(L), writeln(L), fail.
 % [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]
 % [0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0]
@@ -16,17 +17,24 @@
 
 :- use_module(library(clpfd)).
 
-bound(L, M) :-
-  L = [X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, X11, X12, X13, X14, X15, X16],
-  % implications expressed by inequalities (1 - X) * M >= Y, where M - maximum number of ones
-  ((1-X1) * M #>= X9 + X10 + X11 + X12 + X13 + X14 + X15 + X16),
-  ((1-X2) * M #>= X10 + X11 + X12 + X13 + X14 + X15 + X16),
-  ((1-X3) * M #>= X11 + X12 + X13 + X14 + X15 + X16),
-  ((1-X4) * M #>= X12 + X13 + X14 + X15 + X16),
-  ((1-X5) * M #>= X13 + X14 + X15 + X16),
-  ((1-X6) * M #>= X14 + X15 + X16),
-  ((1-X7) * M #>= X15 + X16),
-  ((1-X8) * M #>= X16).
+sum_elements([], _, 0).
+sum_elements([H|T], M, X) :-
+  (
+    M #= 0
+    -> (
+      X #= H + Y, 
+      sum_elements(T, M, Y)
+      ) ; (
+        M #= M1 + 1,
+        sum_elements(T, M1, X)
+      )
+  ).
+
+bound([], _).
+bound([H|T], M) :-
+  sum_elements([H|T], M, X),
+  (1 - H) * M #>= X,
+  bound(T, M).
 
 % implementation using linear bounds 
 linear_interval(List) :-
